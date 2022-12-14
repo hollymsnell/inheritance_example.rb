d3eebb325955f6afbdaf526023d3b90b832c565f
hollymsnell <hollymsnell@gmail.com>
2022-12-14 05:22:06 -0700

    area code dictionary

--- practice.rb
+++ practice.rb
@@ -1,50 +1,40 @@
-users = [
-  { username: "holly", password: "password1" },
-  { username: "santa", password: "clause1" },
-  { username: "summer", password: "comeback"}
-]
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+ 
 
-# index = 0
-# puts "Enter your username"
-# user_input = gets.chomp
-# while index < users.length
-#   if user_input = users[index][:username]
-  
-  
-# end
+def get_city_names(somehash)
+  somehash.keys
+end
+ 
 
-def auth_user(username, password, list_of_users)
-  list_of_users.each do |user_record|
-    if user_record[:username] == username && user_record[:password] == password
-      return user_record
-    end
+def get_area_code(somehash, key)
+  somehash[key]
+end
+ 
+
+loop do
+  puts "Do you want to lookup an area code based on a city name?(Y/N)"
+  answer = gets.chomp.downcase
+  break if answer != "y"
+  puts "Which city do you want the area code for?"
+  puts get_city_names(dial_book)
+  puts "Enter your selection"
+  prompt = gets.chomp.downcase
+  if dial_book.include?(prompt)
+    puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+  else puts "You entered an unlisted name"
   end
-  "Credentials not correct"
 end
-
-puts "welcome to authenticator"
-25.times { print "-"}
-puts 
-puts "This program will take inout from the user and compare password"
-puts "if password is correct, you will get back the user object"
-
-attempts = 1
-while attempts < 4
-  print "Username: "
-  username = gets.chomp
-  print "Password: "
-  password = gets.chomp
-  authentication = auth_user(username, password, users)
-  puts authentication
-  puts "Press n to quit or any other key to continue: "
-  input = gets.chomp.downcase
-  break if input == "n"
-  attempts += 1
-end
-puts "You have exceeded the number of attempts" if attempts == 4
-
-
-
 
 
 
