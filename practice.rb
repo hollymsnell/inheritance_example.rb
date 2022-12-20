86e80c6b78d0a110acf5cffceb583c124c812ce6
hollymsnell <hollymsnell@gmail.com>
2022-12-20 06:35:26 -0700

    OOP review with initialize method, review attribute accessor and reader

--- practice.rb
+++ practice.rb
@@ -1,41 +1,30 @@
-dial_book = {
-  "newyork" => "212",
-  "newbrunswick" => "732",
-  "edison" => "908",
-  "plainsboro" => "609",
-  "sanfrancisco" => "301",
-  "miami" => "305",
-  "paloalto" => "650",
-  "evanston" => "847",
-  "orlando" => "407",
-  "lancaster" => "717"
-}
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
  
-
-def get_city_names(somehash)
-  somehash.keys
-end
- 
-
-def get_area_code(somehash, key)
-  somehash[key]
-end
- 
-
-loop do
-  puts "Do you want to lookup an area code based on a city name?(Y/N)"
-  answer = gets.chomp.downcase
-  break if answer != "y"
-  puts "Which city do you want the area code for?"
-  puts get_city_names(dial_book)
-  puts "Enter your selection"
-  prompt = gets.chomp.downcase
-  if dial_book.include?(prompt)
-    puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
-  else puts "You entered an unlisted name"
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
   end
+ 
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+ 
 end
-
+ 
+mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
+                      "password1")
+john = Student.new("John", "Doe", "john1", "john1@example.com",
+                      "password2")
+puts mashrur
+puts john
+mashrur.last_name = john.last_name
+puts "Mashrur is altered"
+puts mashrur
 
 
 
